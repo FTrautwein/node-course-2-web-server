aeb520fe36799a60448c220677d3640701da8fa9
FTrautwein <ftwein@gmail.com>
2018-06-06 20:53:46 -0300

    Update project page

--- server.js
+++ server.js
@@ -41,15 +41,15 @@ app.get('/', (req, res) => {
   })
 });
 
-app.get('/projects', (req, res) => {
-  res.render('projects.hbs', {
-    pageTitle: 'Projects Page'
-  });
-});
-
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
